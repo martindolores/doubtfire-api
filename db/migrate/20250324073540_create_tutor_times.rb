36a44e7fbcca101d59aee3efd1985c8488e08b17
Martin Dolores <martindolores65@gmail.com>
2025-03-24 18:49:08 +1100

    - remove unnecessary comment from migration

--- db/migrate/20250324073540_create_tutor_times.rb
+++ db/migrate/20250324073540_create_tutor_times.rb
@@ -1,9 +1,9 @@
 class CreateTutorTimes < ActiveRecord::Migration[7.1]
   def change
     create_table :tutor_times do |t|
-      t.references :user, null: false, foreign_key: true  # Links to users table
-      t.references :task, null: false, foreign_key: true  # Links to tasks table
-      t.integer :time_spent, null: false, default: 0       # Time spent in minutes
+      t.references :user, null: false, foreign_key: true
+      t.references :task, null: false, foreign_key: true
+      t.integer :time_spent, null: false, default: 0
       t.timestamps
     end
   end
